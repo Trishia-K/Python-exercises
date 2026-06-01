716b64ba48a276cc106d5b24720abe11fc1b55c6
Kobumanzi Trishia <kobu2rsha@gmail.com>
2026-06-01 14:10:48 +0300

    Basic python practice scripts

---
+++ Foundations practice.py
@@ -0,0 +1,36 @@
+#Day 2
+name= "Trishia Kobumanzi"
+age= 21
+university= "UCU"
+print("My name is", name ,"and I am", age ,"years old." )
+print ("I study at" ,university)
+
+#Day 3
+print("What is your name?")
+name= input()
+print(f"Hey {name}")
+print("Enter number 1:")
+num1=int(input())
+print("Enter number 2")
+num2=int(input())
+sum= (num1 + num2)
+print(f"Sum is {sum}")
+
+#Day 4
+#Using f strings
+
+#Day 5
+if num1>0:
+      print("Number is positive.")
+elif num1<0:
+      print ("Number is negative")
+else:
+    print("zero")
+
+if num2>0:
+      print("Number is positive.")
+elif num2<0:
+      print ("Number is negative")
+else:
+    print("zero")
+
